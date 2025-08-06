37a1a1b2a337e1e34cb4a86c373bfc3f560d3819
contour-gara <65527071+contour-gara@users.noreply.github.com>
2025-08-06 14:50:24 +0900

    可視性のためテストの設定変更

--- discord-bot/build.gradle.kts
+++ discord-bot/build.gradle.kts
@@ -1,3 +1,6 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+
 plugins {
     alias(libs.plugins.kotlin.jvm)
     application
@@ -26,4 +29,19 @@ application {
 
 tasks.named<Test>("test") {
     useJUnitPlatform()
+
+    testLogging {
+        events = setOf(
+            TestLogEvent.FAILED,
+            TestLogEvent.PASSED,
+            TestLogEvent.SKIPPED,
+            TestLogEvent.STANDARD_OUT
+        )
+
+        exceptionFormat = TestExceptionFormat.FULL
+        showStandardStreams = true
+        showExceptions = true
+        showCauses = true
+        showStackTraces = true
+    }
 }
