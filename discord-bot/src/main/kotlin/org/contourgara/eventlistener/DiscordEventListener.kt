f71214c1ad5af9de1a13a5efda458cf32e3b9bd9
contour-gara <65527071+contour-gara@users.noreply.github.com>
2025-08-06 18:12:50 +0900

    テストのモーダル実装

--- discord-bot/src/main/kotlin/org/contourgara/eventlistener/DiscordEventListener.kt
+++ discord-bot/src/main/kotlin/org/contourgara/eventlistener/DiscordEventListener.kt
@@ -1,9 +1,12 @@
 package org.contourgara.eventlistener
 
 import dev.kord.common.entity.Snowflake
+import dev.kord.common.entity.TextInputStyle
 import dev.kord.core.Kord
+import dev.kord.core.behavior.interaction.modal
 import dev.kord.core.behavior.interaction.response.respond
 import dev.kord.core.event.interaction.GuildChatInputCommandInteractionCreateEvent
+import dev.kord.core.event.interaction.ModalSubmitInteractionCreateEvent
 import dev.kord.core.event.message.MessageCreateEvent
 import dev.kord.core.on
 import dev.kord.gateway.Intent
@@ -13,23 +16,39 @@ suspend fun listenDiscordEvent() {
     val kord = Kord(System.getenv("HOME_FINANCE_MANAGER_BOT_TOKEN"))
 
     kord.on<MessageCreateEvent> {
-        println("メッセージ認識")
-        if (message.author?.isBot != false) return@on
+        if (message.author?.isBot!!) return@on
         if (message.content != "タコピー") return@on
-        println("メッセージ条件パス")
         message.channel.createMessage("ハッピーだっピか！？")
     }
 
     kord.createGuildChatInputCommand(
         Snowflake(889318150615744523),
-        "hello",
-        "hello"
+        "modal",
+        "modal test"
     )
 
     kord.on<GuildChatInputCommandInteractionCreateEvent> {
-        println("コマンド実行を認識")
-        val response = interaction.deferPublicResponse()
-        response.respond { content = "たこぴーだっぴ" }
+        if (interaction.invokedCommandName == "modal") {
+            if (interaction.channel.id != Snowflake(1402331708459581591)) interaction.deferPublicResponse().respond { content = "test で実行してね" }
+            interaction.modal("テストモーダル", "test-modal") {
+                actionRow {
+                    textInput(TextInputStyle.Short, "memo", "メモ") {
+                        placeholder = "メモを入力"
+                        allowedLength = 1..100
+                    }
+                }
+            }
+        }
+    }
+
+    kord.on<ModalSubmitInteractionCreateEvent> {
+        when (interaction.modalId) {
+            "test-modal" -> {
+                val memo = interaction.textInputs["memo"]?.value
+                println(memo)
+                interaction.deferPublicResponse().respond { content = "受け付けたっぴ: $memo" }
+            }
+        }
     }
 
     kord.login {
