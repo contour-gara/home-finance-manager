483a52d545d47e3030f31270bebb782547f30251
contour-gara <65527071+contour-gara@users.noreply.github.com>
2025-09-23 08:35:03 +0900

    add next-ulid endpoint and corresponding test case

--- ulid-sequencer/src/main/kotlin/org/contourgara/presentation/Routing.kt
+++ ulid-sequencer/src/main/kotlin/org/contourgara/presentation/Routing.kt
@@ -4,11 +4,16 @@ import io.ktor.server.application.Application
 import io.ktor.server.response.respondText
 import io.ktor.server.routing.get
 import io.ktor.server.routing.routing
+import ulid.ULID
 
 fun Application.configureRouting() {
     routing {
         get("/") {
             call.respondText("Hello World!")
+        }
+
+        get("/next-ulid") {
+            call.respondText(ULID.nextULID().toString())
         }
     }
 }
--- ulid-sequencer/src/test/kotlin/org/contourgara/UlidSequencerApplicationTest.kt
+++ ulid-sequencer/src/test/kotlin/org/contourgara/UlidSequencerApplicationTest.kt
@@ -2,13 +2,16 @@ package org.contourgara
 
 import io.kotest.assertions.ktor.client.shouldHaveStatus
 import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.nulls.shouldNotBeNull
 import io.kotest.matchers.shouldBe
+import io.kotest.matchers.shouldNotBe
 import io.ktor.client.request.get
 import io.ktor.client.statement.bodyAsText
 import io.ktor.server.testing.testApplication
+import ulid.ULID
 
 class UlidSequencerApplicationTest : FunSpec({
-    test("test") {
+    test("ルートエンドポイントにアクセスすると、'Hello World!' が取得できる") {
         testApplication {
             // setup
             application {
@@ -21,6 +24,22 @@ class UlidSequencerApplicationTest : FunSpec({
             // assert
             actual shouldHaveStatus 200
             actual.bodyAsText() shouldBe "Hello World!"
+        }
+    }
+
+    test("next-ulid エンドポイントにアクセスすると、ULID 形式の文字列が取得できる") {
+        testApplication {
+            // setup
+            application {
+                module()
+            }
+
+            // execute
+            val actual = client.get("/next-ulid")
+
+            // assert
+            actual shouldHaveStatus 200
+            ULID.parseULID(actual.bodyAsText()).shouldNotBeNull()
         }
     }
 })
