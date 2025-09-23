8672dd2b4dac85333a34080c6df1d535e752dac4
contour-gara <65527071+contour-gara@users.noreply.github.com>
2025-09-24 03:06:39 +0900

    add environment variable test for Kotest

---
+++ ulid-sequencer/src/test/kotlin/org/contourgara/EnvironmentVariableTest.kt
@@ -0,0 +1,21 @@
+package org.contourgara
+
+import io.kotest.assertions.assertSoftly
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.extensions.system.withEnvironment
+import io.kotest.matchers.shouldBe
+
+class EnvironmentVariableTest : FunSpec({
+    test("環境変数の学習用テスト (jvmArgs の指定が必要)") {
+        withEnvironment(mapOf(
+            "AAA" to "aaa",
+            "BBB" to "bbb",
+        )) {
+            // assert
+            assertSoftly {
+                System.getenv("AAA") shouldBe "aaa"
+                System.getenv("BBB") shouldBe "bbb"
+            }
+        }
+    }
+})
