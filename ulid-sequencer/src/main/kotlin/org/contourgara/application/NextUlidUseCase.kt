ea1c389c2ecf1adb19c3333cf21489a4c9620148
contour-gara <65527071+contour-gara@users.noreply.github.com>
2025-09-25 02:15:12 +0900

    simplify nextUlid function implementation for improved readability

--- ulid-sequencer/src/main/kotlin/org/contourgara/application/NextUlidUseCase.kt
+++ ulid-sequencer/src/main/kotlin/org/contourgara/application/NextUlidUseCase.kt
@@ -2,8 +2,7 @@ package org.contourgara.application
 
 import ulid.ULID
 
-fun nextUlid(latestUlid: () -> ULID, saveUlid: (ULID) -> Unit): ULID {
-    val ulid = ULID.Monotonic.nextULID(latestUlid())
-    saveUlid(ulid)
-    return ulid
-}
+fun nextUlid(latestUlid: () -> ULID, saveUlid: (ULID) -> Unit): ULID =
+    ULID.Monotonic
+        .nextULID(latestUlid())
+        .also { saveUlid(it) }
