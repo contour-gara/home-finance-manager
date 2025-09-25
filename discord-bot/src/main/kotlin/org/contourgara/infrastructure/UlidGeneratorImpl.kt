54a70127b02df38740359403a143b38a2dfbfe69
contour-gara <65527071+contour-gara@users.noreply.github.com>
2025-09-26 00:57:32 +0900

    ログ関係の依存を整理

--- discord-bot/src/main/kotlin/org/contourgara/infrastructure/UlidGeneratorImpl.kt
+++ discord-bot/src/main/kotlin/org/contourgara/infrastructure/UlidGeneratorImpl.kt
@@ -3,6 +3,10 @@ package org.contourgara.infrastructure
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.engine.cio.CIO
+import io.ktor.client.plugins.logging.DEFAULT
+import io.ktor.client.plugins.logging.Logging
+import io.ktor.client.plugins.logging.Logger
+import io.ktor.client.plugins.logging.LogLevel
 import io.ktor.client.request.get
 import kotlinx.coroutines.runBlocking
 import org.contourgara.DiscordBotConfig
@@ -13,7 +17,12 @@ import ulid.ULID
 @Single
 class UlidGeneratorImpl(private val discordBotConfig: DiscordBotConfig) : UlidGenerator {
     override fun nextUlid(): ULID = runBlocking {
-        HttpClient(CIO).use {
+        HttpClient(CIO) {
+            install(Logging) {
+                logger = Logger.DEFAULT
+                level = LogLevel.ALL
+            }
+        }.use {
             it.get("${discordBotConfig.ulidSequencerBaseUrl}/next-ulid")
                 .body<String>()
                 .let { ULID.parseULID(it) }
