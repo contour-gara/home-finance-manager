92d95300c440addbf6c6bff7a4c9acce9c7519a8
contour-gara <65527071+contour-gara@users.noreply.github.com>
2025-09-27 03:18:07 +0900

    refactor RegisterBillUseCase to streamline ULID generation and improve readability

--- discord-bot/src/main/kotlin/org/contourgara/application/RegisterBillUseCase.kt
+++ discord-bot/src/main/kotlin/org/contourgara/application/RegisterBillUseCase.kt
@@ -7,14 +7,16 @@ import org.koin.core.annotation.Single
 @Single
 class RegisterBillUseCase(private val ulidGenerator: UlidGenerator) {
     fun execute(param: RegisterBillParam): RegisterBillDto =
-        Bill.of(
-            id = ulidGenerator.nextUlid(),
-            amount = param.amount,
-            lenderName = param.lender,
-            borrowerName = param.borrower,
-            memo = param.memo,
-        ).let {
-            println("execute UseCase")
-            RegisterBillDto.from(it)
-        }
+        ulidGenerator.nextUlid()
+            .let {
+                Bill.of(
+                    id = it,
+                    amount = param.amount,
+                    lenderName = param.lender,
+                    borrowerName = param.borrower,
+                    memo = param.memo,
+                )
+            }
+            .let { RegisterBillDto.from(it) }
+            .also { println("execute UseCase") }
 }
