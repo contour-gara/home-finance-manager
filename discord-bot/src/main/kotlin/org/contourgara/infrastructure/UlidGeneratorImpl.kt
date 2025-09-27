d962ffe2d6c19ca874b4f6a93130eb3e223ba35e
contour-gara <65527071+contour-gara@users.noreply.github.com>
2025-09-28 00:28:40 +0900

    refactor UlidGeneratorImpl to use bodyAsText for HTTP response handling

--- discord-bot/src/main/kotlin/org/contourgara/infrastructure/UlidGeneratorImpl.kt
+++ discord-bot/src/main/kotlin/org/contourgara/infrastructure/UlidGeneratorImpl.kt
@@ -1,13 +1,13 @@
 package org.contourgara.infrastructure
 
 import io.ktor.client.HttpClient
-import io.ktor.client.call.body
 import io.ktor.client.engine.cio.CIO
 import io.ktor.client.plugins.logging.DEFAULT
 import io.ktor.client.plugins.logging.Logging
 import io.ktor.client.plugins.logging.Logger
 import io.ktor.client.plugins.logging.LogLevel
 import io.ktor.client.request.get
+import io.ktor.client.statement.bodyAsText
 import kotlinx.coroutines.runBlocking
 import org.contourgara.DiscordBotConfig
 import org.contourgara.domain.UlidGenerator
@@ -24,7 +24,7 @@ class UlidGeneratorImpl(private val discordBotConfig: DiscordBotConfig) : UlidGe
             }
         }.use {
             it.get("${discordBotConfig.ulidSequencerBaseUrl}/next-ulid")
-                .body<String>()
+                .bodyAsText()
                 .let { ULID.parseULID(it) }
         }
     }
