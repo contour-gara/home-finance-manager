ea8150c63dbab6d5a6245f11c36c8d75dfe518ba
contour-gara <65527071+contour-gara@users.noreply.github.com>
2025-11-03 02:01:57 +0900

    EngineMain を import するように修正

--- ulid-sequencer/src/main/kotlin/org/contourgara/UlidSequencerApplication.kt
+++ ulid-sequencer/src/main/kotlin/org/contourgara/UlidSequencerApplication.kt
@@ -1,11 +1,12 @@
 package org.contourgara
 
 import io.ktor.server.application.Application
+import io.ktor.server.netty.EngineMain
 import org.contourgara.presentation.configureRouting
 import org.contourgara.repository.migration
 
 fun main(args: Array<String>) {
-    io.ktor.server.netty.EngineMain.main(args)
+    EngineMain.main(args)
 }
 
 fun Application.module() {
