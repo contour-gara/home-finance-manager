d517bf2d21d60cb2ba322e131ee76628f0dca9b0
contour-gara <65527071+contour-gara@users.noreply.github.com>
2025-12-15 11:14:48 +0900

    出費関連のテーブルを作成

--- expenses-api/src/main/resources/db/migration/V1.0.0__initial_schema.sql
+++ expenses-api/src/main/resources/db/migration/V1.0.0__initial_schema.sql
@@ -1,7 +1,58 @@
-CREATE TABLE IF NOT EXISTS balance (
-    balance_id VARCHAR(26) PRIMARY KEY,
-    lender VARCHAR(100) NOT NULL,
-    borrower VARCHAR(100) NOT NULL,
+CREATE TABLE IF NOT EXISTS expense_id (
+    expense_id VARCHAR(26) NOT NULL,
+    PRIMARY KEY(expense_id)
+);
+
+CREATE TABLE IF NOT EXISTS expense_amount (
+    expense_id VARCHAR(26) NOT NULL,
     amount INTEGER NOT NULL,
-    last_event_id VARCHAR(255) NOT NULL
+    PRIMARY KEY(expense_id),
+    FOREIGN KEY(expense_id) REFERENCES expense_id(expense_id)
+);
+
+CREATE TABLE IF NOT EXISTS payer (
+    payer VARCHAR(100) NOT NULL,
+    PRIMARY KEY(payer)
+);
+
+CREATE TABLE IF NOT EXISTS expense_payer (
+    expense_id VARCHAR(26) NOT NULL,
+    payer VARCHAR(100) NOT NULL,
+    PRIMARY KEY(expense_id),
+    FOREIGN KEY(expense_id) REFERENCES expense_id(expense_id),
+    FOREIGN KEY(payer) REFERENCES payer(payer)
+);
+
+CREATE TABLE IF NOT EXISTS category (
+    category VARCHAR(100) NOT NULL,
+    PRIMARY KEY(category)
+);
+
+CREATE TABLE IF NOT EXISTS expense_category (
+    expense_id VARCHAR(26) NOT NULL,
+    category VARCHAR(100) NOT NULL,
+    PRIMARY KEY(expense_id),
+    FOREIGN KEY(expense_id) REFERENCES expense_id(expense_id),
+    FOREIGN KEY(category) REFERENCES category(category)
+);
+
+CREATE TABLE IF NOT EXISTS expense_memo (
+    expense_id VARCHAR(26) NOT NULL,
+    memo VARCHAR(100) NOT NULL,
+    PRIMARY KEY(expense_id),
+    FOREIGN KEY(expense_id) REFERENCES expense_id(expense_id)
+);
+
+CREATE TABLE IF NOT EXISTS expense_create_event (
+    create_event_id VARCHAR(26) NOT NULL,
+    expense_id VARCHAR(26) NOT NULL,
+    PRIMARY KEY(create_event_id),
+    FOREIGN KEY(expense_id) REFERENCES expense_id(expense_id)
+);
+
+CREATE TABLE IF NOT EXISTS expense_delete_event (
+    delete_event_id VARCHAR(26) NOT NULL,
+    expense_id VARCHAR(26) NOT NULL,
+    PRIMARY KEY(delete_event_id),
+    FOREIGN KEY(expense_id) REFERENCES expense_id(expense_id)
 );
---
+++ expenses-api/src/main/resources/db/migration/V2.0.0__initial_data.sql
@@ -0,0 +1,18 @@
+INSERT INTO payer
+    (payer)
+    VALUES
+        ('GARA'),
+        ('YUKI'),
+        ('DIRECT_DEBIT');
+
+INSERT INTO category
+    (category)
+    VALUES
+        ('RENT'),
+        ('UTILITIES'),
+        ('FOOD'),
+        ('DAILY_NEEDS'),
+        ('HEALTHCARE'),
+        ('ENTERTAINMENT'),
+        ('TRAVEL'),
+        ('OTHER');
