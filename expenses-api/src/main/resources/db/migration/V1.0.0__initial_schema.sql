570f47f14154a8298ccfc8fdf6c2f830d00263af
contour-gara <65527071+contour-gara@users.noreply.github.com>
2025-12-22 15:29:39 +0900

    支出イベント ID テーブルの名前変更

--- expenses-api/src/main/resources/db/migration/V1.0.0__initial_schema.sql
+++ expenses-api/src/main/resources/db/migration/V1.0.0__initial_schema.sql
@@ -43,16 +43,16 @@ CREATE TABLE IF NOT EXISTS expense_memo (
     FOREIGN KEY(expense_id) REFERENCES expense_id(expense_id)
 );
 
-CREATE TABLE IF NOT EXISTS event_id (
-    event_id VARCHAR(26) NOT NULL,
-    PRIMARY KEY(event_id)
+CREATE TABLE IF NOT EXISTS expense_event_id (
+    expense_event_id VARCHAR(26) NOT NULL,
+    PRIMARY KEY(expense_event_id)
 );
 
 CREATE TABLE IF NOT EXISTS expense_event (
-    event_id VARCHAR(26) NOT NULL,
+    expense_event_id VARCHAR(26) NOT NULL,
     expense_id VARCHAR(26) NOT NULL,
-    PRIMARY KEY(event_id),
-    FOREIGN KEY(event_id) REFERENCES event_id(event_id),
+    PRIMARY KEY(expense_event_id),
+    FOREIGN KEY(expense_event_id) REFERENCES expense_event_id(expense_event_id),
     FOREIGN KEY(expense_id) REFERENCES expense_id(expense_id)
 );
 
@@ -62,9 +62,9 @@ CREATE TABLE IF NOT EXISTS event_category (
 );
 
 CREATE TABLE IF NOT EXISTS expense_event_category (
-    event_id VARCHAR(26) NOT NULL,
+    expense_event_id VARCHAR(26) NOT NULL,
     event_category VARCHAR(26) NOT NULL,
-    PRIMARY KEY(event_id),
-    FOREIGN KEY(event_id) REFERENCES event_id(event_id),
+    PRIMARY KEY(expense_event_id),
+    FOREIGN KEY(expense_event_id) REFERENCES expense_event_id(expense_event_id),
     FOREIGN KEY(event_category) REFERENCES event_category(event_category)
 );
