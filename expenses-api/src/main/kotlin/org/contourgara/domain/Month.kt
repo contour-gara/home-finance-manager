2d3252e51b1b46c1c13c089f41c2c8d4703200ee
contour-gara <65527071+contour-gara@users.noreply.github.com>
2025-12-24 22:17:45 +0900

    有効な月のドメインモデルを実装

---
+++ expenses-api/src/main/kotlin/org/contourgara/domain/Month.kt
@@ -0,0 +1,24 @@
+package org.contourgara.domain
+
+enum class Month(private val intMonth: Int) {
+    JANUARY(intMonth = 1),
+    FEBRUARY(intMonth = 2),
+    MARCH(intMonth = 3),
+    APRIL(intMonth = 4),
+    MAY(intMonth = 5),
+    JUNE(intMonth = 6),
+    JULY(intMonth = 7),
+    AUGUST(intMonth = 8),
+    SEPTEMBER(intMonth = 9),
+    OCTOBER(intMonth = 10),
+    NOVEMBER(intMonth = 11),
+    DECEMBER(intMonth = 12),
+    ;
+
+    companion object {
+        fun of(intMonth: Int): Month =
+            entries.firstOrNull {
+                it.intMonth == intMonth
+            } ?: throw IllegalArgumentException("Not found: $intMonth")
+    }
+}
---
+++ expenses-api/src/test/kotlin/org/contourgara/domain/MonthTest.kt
@@ -0,0 +1,23 @@
+package org.contourgara.domain
+
+import io.kotest.assertions.throwables.shouldThrowExactly
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
+
+class MonthTest : FunSpec({
+    test("Int から Month を取得できる") {
+        // execute
+        val actual = Month.of(intMonth = 1)
+
+        // assert
+        val expected = Month.JANUARY
+        actual shouldBe expected
+    }
+
+    test("Int から Month 取得で不適切な値の場合は例外が投げられる") {
+        // execute & assert
+        shouldThrowExactly<IllegalArgumentException> {
+            Month.of(intMonth = 13)
+        }.message shouldBe "Not found: 13"
+    }
+})
