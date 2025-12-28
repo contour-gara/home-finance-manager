8249c0341f4f226962b6332e90cd9620c2043b07
contour-gara <65527071+contour-gara@users.noreply.github.com>
2025-12-28 15:31:23 +0900

    複数テストに対応しやすいようにリファクタリング

--- expenses-api/src/test/kotlin/org/contourgara/infrastructure/repository/ExpensesRepositoryImplTest.kt
+++ expenses-api/src/test/kotlin/org/contourgara/infrastructure/repository/ExpensesRepositoryImplTest.kt
@@ -3,7 +3,9 @@ package org.contourgara.infrastructure.repository
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.nulls.shouldBeNull
 import org.assertj.db.api.Assertions.assertThat
+import org.assertj.db.type.AssertDbConnection
 import org.assertj.db.type.AssertDbConnectionFactory
+import org.assertj.db.type.Table
 import org.contourgara.domain.Category
 import org.contourgara.domain.Month
 import org.contourgara.domain.Payer
@@ -13,6 +15,15 @@ import org.jetbrains.exposed.v1.jdbc.Database
 import org.testcontainers.containers.MySQLContainer
 
 class ExpensesRepositoryImplTest : FunSpec({
+    lateinit var assertDbConnection: AssertDbConnection
+    lateinit var expensesYearTable: Table
+    lateinit var expensesMonthTable: Table
+    lateinit var expensesPayerTable: Table
+    lateinit var expensesCategoryTable: Table
+    lateinit var expensesAmountTable: Table
+
+    val sut = ExpensesRepositoryImpl
+
     val mysql = MySQLContainer("mysql:8.0.43-oraclelinux9").apply {
         startupAttempts = 1
     }
@@ -37,6 +48,13 @@ class ExpensesRepositoryImplTest : FunSpec({
             user = mysql.username,
             password = mysql.password,
         )
+
+        assertDbConnection = AssertDbConnectionFactory.of(mysql.jdbcUrl, mysql.username, mysql.password).create()
+        expensesYearTable = assertDbConnection.table("expenses_year").build()
+        expensesMonthTable = assertDbConnection.table("expenses_month").build()
+        expensesPayerTable = assertDbConnection.table("expenses_payer").build()
+        expensesCategoryTable = assertDbConnection.table("expenses_category").build()
+        expensesAmountTable = assertDbConnection.table("expenses_amount").build()
     }
 
     beforeTest {
@@ -44,15 +62,6 @@ class ExpensesRepositoryImplTest : FunSpec({
     }
 
     test("支出合計が存在しない場合、null を返す") {
-        val assertDbConnection = AssertDbConnectionFactory.of(mysql.jdbcUrl, mysql.username, mysql.password).create()
-        val expensesYearTable = assertDbConnection.table("expenses_year").build()
-        val expensesMonthTable = assertDbConnection.table("expenses_month").build()
-        val expensesPayerTable = assertDbConnection.table("expenses_payer").build()
-        val expensesCategoryTable = assertDbConnection.table("expenses_category").build()
-        val expensesAmountTable = assertDbConnection.table("expenses_amount").build()
-
-        val sut = ExpensesRepositoryImpl
-
         // execute
         val actual = sut.findLatestExpenses(Year._2026, Month.JANUARY, Payer.DIRECT_DEBIT, Category.RENT)
 
