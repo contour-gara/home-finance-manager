285ffa77ce9b85e0940eff61943911d236977a81
contour-gara <65527071+contour-gara@users.noreply.github.com>
2025-12-29 00:05:15 +0900

    支出合計更新の際の例外処理を実装

--- expenses-api/src/main/kotlin/org/contourgara/domain/Expenses.kt
+++ expenses-api/src/main/kotlin/org/contourgara/domain/Expenses.kt
@@ -11,7 +11,13 @@ data class Expenses(
     companion object {
         fun from(expenses: Expenses?, expense: Expense, expenseEventId: ExpenseEventId) : Expenses =
             expenses
-                ?.let {
+                ?.also {
+                    require(expenses.lastEventId < expenseEventId) { "expenseEventId must be greater than lastEventId: expenseEventId = ${expenseEventId.id}, lastEventId = ${expenses.lastEventId.id}" }
+                    require(expenses.year == expense.year) { "year must be same: expenses.year = ${expenses.year.intYear}, expense.year = ${expense.year.intYear}" }
+                    require(expenses.month == expense.month) { "month must be same: expenses.month = ${expenses.month.intMonth}, expense.month = ${expense.month.intMonth}" }
+                    require(expenses.payer == expense.payer) { "payer must be same: expenses.payer = ${expenses.payer.name}, expense.payer = ${expense.payer.name}" }
+                    require(expenses.category == expense.category) { "category must be same: expenses.category = ${expenses.category.name}, expense.category = ${expense.category.name}" }
+                } ?.let {
                     expenses.copy(
                         lastEventId = expenseEventId,
                         amount = it.amount + expense.amount,
--- expenses-api/src/test/kotlin/org/contourgara/domain/ExpensesTest.kt
+++ expenses-api/src/test/kotlin/org/contourgara/domain/ExpensesTest.kt
@@ -1,5 +1,6 @@
 package org.contourgara.domain
 
+import io.kotest.assertions.throwables.shouldThrowExactly
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
 import ulid.ULID
@@ -9,10 +10,10 @@ class ExpensesTest : FunSpec({
         // setup
         val expenses = Expenses(
             lastEventId = ExpenseEventId(id = ULID.parseULID("01KD27JEZQQY88RG18034YZHBV")),
-            payer = Payer.DIRECT_DEBIT,
-            category = Category.RENT,
             year = Year._2026,
             month = Month.JANUARY,
+            payer = Payer.DIRECT_DEBIT,
+            category = Category.RENT,
             amount = 1000,
         )
 
@@ -34,10 +35,10 @@ class ExpensesTest : FunSpec({
         // assert
         val expected = Expenses(
             lastEventId = ExpenseEventId(id = ULID.parseULID("01KDJBSGQSV39NEGEQGXPH350Y")),
-            payer = Payer.DIRECT_DEBIT,
-            category = Category.RENT,
             year = Year._2026,
             month = Month.JANUARY,
+            payer = Payer.DIRECT_DEBIT,
+            category = Category.RENT,
             amount = 1500,
         )
         actual shouldBe expected
@@ -65,12 +66,157 @@ class ExpensesTest : FunSpec({
         // assert
         val expected = Expenses(
             lastEventId = ExpenseEventId(id = ULID.parseULID("01KDJBSGQSV39NEGEQGXPH350Y")),
+            year = Year._2026,
+            month = Month.JANUARY,
+            payer = Payer.DIRECT_DEBIT,
+            category = Category.RENT,
+            amount = 500,
+        )
+        actual shouldBe expected
+    }
+
+    test("支出合計のイベント ID よりもイベント ID が古い場合、例外を投げる") {
+        // setup
+        val expenses = Expenses(
+            lastEventId = ExpenseEventId(id = ULID.parseULID("01KDJBSGQSV39NEGEQGXPH350Y")),
+            year = Year._2026,
+            month = Month.JANUARY,
+            payer = Payer.DIRECT_DEBIT,
+            category = Category.RENT,
+            amount = 1000,
+        )
+
+        val expense = Expense(
+            expenseId = ExpenseId(id = ULID.parseULID("01KDJ042GNBA94CWB2F151W2SE")),
+            amount = 500,
             payer = Payer.DIRECT_DEBIT,
             category = Category.RENT,
             year = Year._2026,
             month = Month.JANUARY,
-            amount = 500,
+            memo = "test1",
         )
-        actual shouldBe expected
+
+        val expenseEventId = ExpenseEventId(id = ULID.parseULID("01KD27JEZQQY88RG18034YZHBV"))
+
+        // execute & assert
+        shouldThrowExactly<IllegalArgumentException> {
+            Expenses.from(expenses, expense, expenseEventId)
+        }.message shouldBe "expenseEventId must be greater than lastEventId: expenseEventId = 01KD27JEZQQY88RG18034YZHBV, lastEventId = 01KDJBSGQSV39NEGEQGXPH350Y"
+    }
+
+    test("支出合計と支出の年が違う場合、例外を投げる") {
+        // setup
+        val expenses = Expenses(
+            lastEventId = ExpenseEventId(id = ULID.parseULID("01KD27JEZQQY88RG18034YZHBV")),
+            year = Year._2027,
+            month = Month.JANUARY,
+            payer = Payer.DIRECT_DEBIT,
+            category = Category.RENT,
+            amount = 1000,
+        )
+
+        val expense = Expense(
+            expenseId = ExpenseId(id = ULID.parseULID("01KDJ042GNBA94CWB2F151W2SE")),
+            amount = 500,
+            payer = Payer.DIRECT_DEBIT,
+            category = Category.RENT,
+            year = Year._2026,
+            month = Month.JANUARY,
+            memo = "test1",
+        )
+
+        val expenseEventId = ExpenseEventId(id = ULID.parseULID("01KDJBSGQSV39NEGEQGXPH350Y"))
+
+        // execute & assert
+        shouldThrowExactly<IllegalArgumentException> {
+            Expenses.from(expenses, expense, expenseEventId)
+        }.message shouldBe "year must be same: expenses.year = 2027, expense.year = 2026"
+    }
+
+    test("支出合計と支出の月が違う場合、例外を投げる") {
+        // setup
+        val expenses = Expenses(
+            lastEventId = ExpenseEventId(id = ULID.parseULID("01KD27JEZQQY88RG18034YZHBV")),
+            year = Year._2026,
+            month = Month.FEBRUARY,
+            payer = Payer.DIRECT_DEBIT,
+            category = Category.RENT,
+            amount = 1000,
+        )
+
+        val expense = Expense(
+            expenseId = ExpenseId(id = ULID.parseULID("01KDJ042GNBA94CWB2F151W2SE")),
+            amount = 500,
+            payer = Payer.DIRECT_DEBIT,
+            category = Category.RENT,
+            year = Year._2026,
+            month = Month.JANUARY,
+            memo = "test1",
+        )
+
+        val expenseEventId = ExpenseEventId(id = ULID.parseULID("01KDJBSGQSV39NEGEQGXPH350Y"))
+
+        // execute & assert
+        shouldThrowExactly<IllegalArgumentException> {
+            Expenses.from(expenses, expense, expenseEventId)
+        }.message shouldBe "month must be same: expenses.month = 2, expense.month = 1"
+    }
+
+    test("支出合計と支出の支出者が違う場合、例外を投げる") {
+        // setup
+        val expenses = Expenses(
+            lastEventId = ExpenseEventId(id = ULID.parseULID("01KD27JEZQQY88RG18034YZHBV")),
+            year = Year._2026,
+            month = Month.JANUARY,
+            payer = Payer.GARA,
+            category = Category.RENT,
+            amount = 1000,
+        )
+
+        val expense = Expense(
+            expenseId = ExpenseId(id = ULID.parseULID("01KDJ042GNBA94CWB2F151W2SE")),
+            amount = 500,
+            payer = Payer.DIRECT_DEBIT,
+            category = Category.RENT,
+            year = Year._2026,
+            month = Month.JANUARY,
+            memo = "test1",
+        )
+
+        val expenseEventId = ExpenseEventId(id = ULID.parseULID("01KDJBSGQSV39NEGEQGXPH350Y"))
+
+        // execute & assert
+        shouldThrowExactly<IllegalArgumentException> {
+            Expenses.from(expenses, expense, expenseEventId)
+        }.message shouldBe "payer must be same: expenses.payer = GARA, expense.payer = DIRECT_DEBIT"
+    }
+
+    test("支出合計と支出の支出カテゴリが違う場合、例外を投げる") {
+        // setup
+        val expenses = Expenses(
+            lastEventId = ExpenseEventId(id = ULID.parseULID("01KD27JEZQQY88RG18034YZHBV")),
+            year = Year._2026,
+            month = Month.JANUARY,
+            payer = Payer.DIRECT_DEBIT,
+            category = Category.UTILITIES,
+            amount = 1000,
+        )
+
+        val expense = Expense(
+            expenseId = ExpenseId(id = ULID.parseULID("01KDJ042GNBA94CWB2F151W2SE")),
+            amount = 500,
+            payer = Payer.DIRECT_DEBIT,
+            category = Category.RENT,
+            year = Year._2026,
+            month = Month.JANUARY,
+            memo = "test1",
+        )
+
+        val expenseEventId = ExpenseEventId(id = ULID.parseULID("01KDJBSGQSV39NEGEQGXPH350Y"))
+
+        // execute & assert
+        shouldThrowExactly<IllegalArgumentException> {
+            Expenses.from(expenses, expense, expenseEventId)
+        }.message shouldBe "category must be same: expenses.category = UTILITIES, expense.category = RENT"
     }
 })
