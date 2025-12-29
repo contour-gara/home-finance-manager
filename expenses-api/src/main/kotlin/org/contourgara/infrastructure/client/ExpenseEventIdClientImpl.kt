6f963b449f06b8bbb2b6e7f9076b18b2a7a5d680
contour-gara <65527071+contour-gara@users.noreply.github.com>
2025-12-29 13:14:49 +0900

    HttpClient のログレベルを Debug に変更

--- expenses-api/src/main/kotlin/org/contourgara/infrastructure/client/ExpenseEventIdClientImpl.kt
+++ expenses-api/src/main/kotlin/org/contourgara/infrastructure/client/ExpenseEventIdClientImpl.kt
@@ -2,7 +2,6 @@ package org.contourgara.infrastructure.client
 
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.cio.CIO
-import io.ktor.client.plugins.logging.DEFAULT
 import io.ktor.client.plugins.logging.LogLevel
 import io.ktor.client.plugins.logging.Logger
 import io.ktor.client.plugins.logging.Logging
@@ -12,6 +11,7 @@ import kotlinx.coroutines.runBlocking
 import org.contourgara.AppConfig
 import org.contourgara.domain.ExpenseEventId
 import org.contourgara.domain.infrastructure.ExpenseEventIdClient
+import org.slf4j.LoggerFactory
 import ulid.ULID
 
 class ExpenseEventIdClientImpl(
@@ -21,7 +21,11 @@ class ExpenseEventIdClientImpl(
         runBlocking {
             HttpClient(CIO) {
                 install(Logging) {
-                    logger = Logger.DEFAULT
+                    logger = object : Logger {
+                        override fun log(message: String) {
+                            LoggerFactory.getLogger(HttpClient::class.java).debug(message)
+                        }
+                    }
                     level = LogLevel.ALL
                 }
             }.use {
