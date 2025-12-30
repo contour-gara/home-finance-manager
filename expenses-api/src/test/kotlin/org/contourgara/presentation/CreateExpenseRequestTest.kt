2538e335118a7354e1e1216b2f22ed388e779eec
contour-gara <65527071+contour-gara@users.noreply.github.com>
2025-12-30 15:15:38 +0900

    JSON のパースパターンを追加

--- expenses-api/src/test/kotlin/org/contourgara/presentation/CreateExpenseRequestTest.kt
+++ expenses-api/src/test/kotlin/org/contourgara/presentation/CreateExpenseRequestTest.kt
@@ -1,29 +1,100 @@
 package org.contourgara.presentation
 
 import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.assertions.throwables.shouldThrowExactly
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.MissingFieldException
 import kotlinx.serialization.SerializationException
 import kotlinx.serialization.json.Json
 
+@OptIn(ExperimentalSerializationApi::class)
 class CreateExpenseRequestTest : FunSpec({
-    test("json パースできなかった場合の例外") {
+    test("フィールドが足りない場合、MissingFieldException が発生すること") {
         // setup
         val json = """
-            
+            {
+                "expenseId": "01K4MXEKC0PMTJ8FA055N4SH79",
+                "amount": 1000,
+                "payer":"DIRECT_DEBIT",
+                "category":"RENT",
+                "year":"2026"
+            }
+        """.trimIndent()
+
+        // execute & assert
+        val e = shouldThrowExactly<MissingFieldException> {
+            Json.decodeFromString<CreateExpenseRequest>(string = json)
+        }
+        e.message shouldBe "Fields [month, memo] are required for type with serial name 'org.contourgara.presentation.CreateExpenseRequest', but they were missing at path: \$"
+        e.missingFields shouldBe listOf("month", "memo")
+    }
+
+    test("Int のフィールドに文字列が入っている場合、SerializationException が発生すること") {
+        // setup
+        val json = """
+            {
+                "expenseId": "01K4MXEKC0PMTJ8FA055N4SH79",
+                "amount": "a",
+                "payer":"DIRECT_DEBIT",
+                "category":"RENT",
+                "year":"2026",
+                "month": "5 月",
+                "memo": "test"
+            }
+        """.trimIndent()
+
+        // execute & assert
+        val e = shouldThrow<SerializationException> {
+            Json.decodeFromString<CreateExpenseRequest>(string = json)
+        }
+        e.message shouldBe """
+            Unexpected JSON token at offset 63: Unexpected symbol 'a' in numeric literal at path: $.amount
+            JSON input: {
+                "expenseId": "01K4MXEKC0PMTJ8FA055N4SH79",
+                "amount": "a",
+                "payer":"DIRECT_DEBIT",
+                "category":"RENT",
+                "year":"2026",
+                "month": "5 月",
+                "memo": "test"
+            }
+        """.trimIndent()
+    }
+
+    test("不要なフィールドが入っている場合、SerializationException が発生すること") {
+        // setup
+        val json = """
             {
                 "expenseId": "01K4MXEKC0PMTJ8FA055N4SH79",
                 "amount": 1000,
                 "payer":"DIRECT_DEBIT",
                 "category":"RENT",
                 "year":"2026",
-                "month":"1"
+                "month": "5",
+                "memo":"test",
+                "test":"test"
             }
         """.trimIndent()
 
         // execute & assert
-        shouldThrow<SerializationException> {
+        val e = shouldThrow<SerializationException> {
             Json.decodeFromString<CreateExpenseRequest>(string = json)
-        }.message shouldBe "Field 'memo' is required for type with serial name 'org.contourgara.presentation.CreateExpenseRequest', but it was missing at path: \$"
+        }
+        e.message shouldBe """
+            Encountered an unknown key 'test' at offset 181 at path: $
+            Use 'ignoreUnknownKeys = true' in 'Json {}' builder or '@JsonIgnoreUnknownKeys' annotation to ignore unknown keys.
+            JSON input: {
+                "expenseId": "01K4MXEKC0PMTJ8FA055N4SH79",
+                "amount": 1000,
+                "payer":"DIRECT_DEBIT",
+                "category":"RENT",
+                "year":"2026",
+                "month": "5",
+                "memo":"test",
+                "test":"test"
+            }
+        """.trimIndent()
     }
 })
