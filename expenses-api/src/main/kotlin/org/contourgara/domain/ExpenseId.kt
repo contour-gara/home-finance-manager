6db39e9cf0de2420826dcdd3da56ba4de161a5a8
contour-gara <65527071+contour-gara@users.noreply.github.com>
2025-12-31 17:40:48 +0900

    ExpenseId のバリデーション実装

--- expenses-api/src/main/kotlin/org/contourgara/domain/ExpenseId.kt
+++ expenses-api/src/main/kotlin/org/contourgara/domain/ExpenseId.kt
@@ -1,17 +1,15 @@
 package org.contourgara.domain
 
 import arrow.core.EitherNel
-import arrow.core.raise.either
 import ulid.ULID
 
 @JvmInline
 value class ExpenseId(val id: ULID) {
     companion object {
-//        fun of(id: String): EitherNel<String, ExpenseId> =
-//            either {
-//                ULID.of(ulidString = id).bind()
-//                ExpenseId(id)
-//            }
+        fun of(id: String): EitherNel<Error, ExpenseId> =
+            ULID
+                .of(ulidString = id)
+                .map { ExpenseId(id = it) }
     }
 
     constructor(id: String) : this(id = ULID.parseULID(ulidString = id))
---
+++ expenses-api/src/test/kotlin/org/contourgara/domain/ExpenseIdTest.kt
@@ -0,0 +1,49 @@
+package org.contourgara.domain
+
+import io.kotest.assertions.arrow.core.shouldBeLeft
+import io.kotest.assertions.arrow.core.shouldBeRight
+import io.kotest.assertions.arrow.core.shouldHaveSize
+import io.kotest.assertions.assertSoftly
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.datatest.withData
+import io.kotest.engine.names.WithDataTestName
+import io.kotest.matchers.shouldBe
+import ulid.ULID
+
+class ExpenseIdTest : FunSpec({
+    context("ExpenseId のファクトリーメソッドの動作確認") {
+        test("ULID にパースできる場合、right に ExpenseId が返る") {
+            // execute
+            val actual = ExpenseId.of(id = "01K4MXEKC0PMTJ8FA055N4SH79")
+
+            // assert
+            assertSoftly {
+                actual.shouldBeRight()
+                actual.value shouldBe ExpenseId(id = ULID.parseULID("01K4MXEKC0PMTJ8FA055N4SH79"))
+            }
+        }
+
+        data class InvalidUlidTestCase(val id: String) : WithDataTestName {
+            override fun dataTestName(): String = "入力が $id の場合、ValidationError を返す"
+        }
+
+        withData(
+            InvalidUlidTestCase(id = "test"),
+            InvalidUlidTestCase(id = "aaaaaaaaaaaaaaaaaaaaaaaaaa"),
+        ) { (id) ->
+            // execute
+            val actual = ExpenseId.of(id = id)
+
+            // assert
+            assertSoftly {
+                actual.shouldBeLeft()
+                actual.value shouldHaveSize 1
+                actual.value.first() shouldBe ValidationError(
+                    pointer = "/ulid",
+                    invalidValue = id,
+                    detail = "value is not a valid ULID format.",
+                )
+            }
+        }
+    }
+})
