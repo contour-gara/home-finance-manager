9cc6ab57587bbfc411931dde7db056e0c758bb39
contour-gara <65527071+contour-gara@users.noreply.github.com>
2026-01-01 13:44:01 +0900

    detail は共通のため

--- expenses-api/src/main/kotlin/org/contourgara/domain/Error.kt
+++ expenses-api/src/main/kotlin/org/contourgara/domain/Error.kt
@@ -2,15 +2,17 @@ package org.contourgara.domain
 
 sealed interface Error {
     val title: String
+    val detail: String
 }
 
 data class ValidationError(
     override val title: String = "Validation Error",
     val pointer: String,
     val invalidValue: String,
-    val detail: String,
+    override val detail: String,
 ) : Error {
     constructor(pointer: String, invalidValue: Int, detail: String) : this(pointer = pointer, invalidValue = invalidValue.toString(), detail = detail)
 }
 
+// TODO: ExpenseDuplicateError
 // TODO: ExpenseNotFoundError
