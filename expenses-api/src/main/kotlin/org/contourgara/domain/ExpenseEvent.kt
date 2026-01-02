f67dd65958caf4ded89c35c172bf061012f9d03d
contour-gara <65527071+contour-gara@users.noreply.github.com>
2026-01-02 14:40:09 +0900

    古い ULID で削除できないように変更

--- expenses-api/src/main/kotlin/org/contourgara/domain/ExpenseEvent.kt
+++ expenses-api/src/main/kotlin/org/contourgara/domain/ExpenseEvent.kt
@@ -9,16 +9,21 @@ data class ExpenseEvent(
     val expenseId: ExpenseId,
     val eventCategory: EventCategory,
 ) {
-    fun delete(expenseEventId: ExpenseEventId): Either<Error, ExpenseEvent> =
-        either {
-            ensure(condition = eventCategory == EventCategory.CREATE) {
-                ExpenseAlreadyDeletedError(
-                    expenseId = expenseId.value.toString(),
-                )
-            }
-            copy(
-                expenseEventId = expenseEventId,
-                eventCategory = EventCategory.DELETE,
-            )
+    fun delete(deleteEventId: ExpenseEventId): Either<Error, ExpenseEvent> =
+        also {
+            require(this.expenseEventId < deleteEventId) { "deleteEventId must be greater than expenseEventId: deleteEventId = ${deleteEventId.value}, expenseEventId = ${expenseEventId.value}" }
         }
+            .let {
+                either {
+                    ensure(condition = eventCategory == EventCategory.CREATE) {
+                        ExpenseAlreadyDeletedError(
+                            expenseId = expenseId.value.toString(),
+                        )
+                    }
+                    copy(
+                        expenseEventId = deleteEventId,
+                        eventCategory = EventCategory.DELETE,
+                    )
+                }
+            }
 }
--- expenses-api/src/test/kotlin/org/contourgara/domain/ExpenseEventTest.kt
+++ expenses-api/src/test/kotlin/org/contourgara/domain/ExpenseEventTest.kt
@@ -3,6 +3,7 @@ package org.contourgara.domain
 import io.kotest.assertions.arrow.core.shouldBeLeft
 import io.kotest.assertions.arrow.core.shouldBeRight
 import io.kotest.assertions.assertSoftly
+import io.kotest.assertions.throwables.shouldThrowExactly
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
 import ulid.ULID
@@ -19,7 +20,7 @@ class ExpenseEventTest : FunSpec({
         val deleteEventId = ExpenseEventId(value = ULID.parseULID("01KDHVD5XTTR9XR4ZAFSSETGXS"))
 
         // execute
-        val actual = expenseEvent.delete(expenseEventId = deleteEventId)
+        val actual = expenseEvent.delete(deleteEventId = deleteEventId)
 
         // assert
         val expected = ExpenseAlreadyDeletedError(
@@ -29,6 +30,22 @@ class ExpenseEventTest : FunSpec({
             actual.shouldBeLeft()
             actual.value shouldBe expected
         }
+    }
+
+    test("作成イベント ID よりも古い削除イベント ID で削除しようとした場合、IllegalArgumentException が投げれれる") {
+        // setup
+        val expenseEvent = ExpenseEvent(
+            expenseEventId = ExpenseEventId(value = ULID.parseULID("01KDHVD5XTTR9XR4ZAFSSETGXS")),
+            expenseId = ExpenseId(value = "01K4MXEKC0PMTJ8FA055N4SH79"),
+            eventCategory = EventCategory.DELETE,
+        )
+
+        val deleteEventId = ExpenseEventId(value = ULID.parseULID("01KD27JEZQQY88RG18034YZHBV"))
+
+        // execute & assert
+        shouldThrowExactly<IllegalArgumentException> {
+            expenseEvent.delete(deleteEventId = deleteEventId)
+        }.message shouldBe "deleteEventId must be greater than expenseEventId: deleteEventId = 01KD27JEZQQY88RG18034YZHBV, expenseEventId = 01KDHVD5XTTR9XR4ZAFSSETGXS"
     }
 
     test("削除されていない支出を削除しようとした場合、削除済みの ExpenseEvent が返る") {
@@ -42,7 +59,7 @@ class ExpenseEventTest : FunSpec({
         val deleteEventId = ExpenseEventId(value = ULID.parseULID("01KDHVD5XTTR9XR4ZAFSSETGXS"))
 
         // execute
-        val actual = expenseEvent.delete(expenseEventId = deleteEventId)
+        val actual = expenseEvent.delete(deleteEventId = deleteEventId)
 
         // assert
         val expected = ExpenseEvent(
