710c2aa4773c1d2736cf28d5cba24cccac6c9a74
contour-gara <65527071+contour-gara@users.noreply.github.com>
2026-02-09 09:53:16 +0900

    transaction 宣言を忘れていたので追加

--- expenses-api/src/main/kotlin/org/contourgara/application/MonthlyExpensesQueryService.kt
+++ expenses-api/src/main/kotlin/org/contourgara/application/MonthlyExpensesQueryService.kt
@@ -7,23 +7,26 @@ import org.contourgara.domain.Payer
 import org.contourgara.domain.Year
 import org.contourgara.domain.infrastructure.ExpensesRepository
 import org.contourgara.domain.toException
+import org.jetbrains.exposed.v1.jdbc.transactions.transaction
 
 class MonthlyExpensesQueryService(
     private val expensesRepository: ExpensesRepository,
 ) {
     fun execute(year: Int, month: Int, payer: String? = null): Map<String, Int> =
-        expensesRepository
-            .findMonthlyExpenses(year = Year.of(value = year), month = Month.of(value = month))
-            .let {
-                if (payer == null) MonthlyExpenses.from(expenses = it)
-                else when (val result = Payer.of(value = payer)) {
-                    is Either.Right -> MonthlyExpenses.from(expenses = it, payer = result.value)
-                    is Either.Left -> throw result.value.toException()
+        transaction {
+            expensesRepository
+                .findMonthlyExpenses(year = Year.of(value = year), month = Month.of(value = month))
+                .let {
+                    if (payer == null) MonthlyExpenses.from(expenses = it)
+                    else when (val result = Payer.of(value = payer)) {
+                        is Either.Right -> MonthlyExpenses.from(expenses = it, payer = result.value)
+                        is Either.Left -> throw result.value.toException()
+                    }
                 }
-            }
-            .values
-            .entries
-            .associate { (category, amount) ->
-                category.name to amount.value
-            }
+                .values
+                .entries
+                .associate { (category, amount) ->
+                    category.name to amount.value
+                }
+        }
 }
