7eba66f760f9493b7387ba7031a13bebcbb1874c
contour-gara <65527071+contour-gara@users.noreply.github.com>
2026-03-29 12:08:18 +0900

    最新の ULID を検索していることがわかりやすくなるように変更

--- ulid-sequencer/src/main/kotlin/org/contourgara/application/NextUlidUseCase.kt
+++ ulid-sequencer/src/main/kotlin/org/contourgara/application/NextUlidUseCase.kt
@@ -3,9 +3,9 @@ package org.contourgara.application
 import org.jetbrains.exposed.v1.jdbc.transactions.transaction
 import ulid.ULID
 
-fun nextUlid(latestUlid: () -> ULID, saveUlid: (ULID) -> Unit): ULID =
+fun nextUlid(findLatestUlid: () -> ULID, saveUlid: (ULID) -> Unit): ULID =
     transaction {
         ULID.Monotonic
-            .nextULID(latestUlid())
+            .nextULID(findLatestUlid())
             .also { saveUlid(it) }
     }
